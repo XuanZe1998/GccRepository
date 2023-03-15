9060a3be034752eef2ef362db0c6da07e2d4a609
GG <1525365479@qq.com>
2023-03-15 17:03:06 +0800

    初始化工程

---
+++ src/main/java/com/tzp/materials/MaterialsApplication.java
@@ -0,0 +1,18 @@
+package com.tzp.materials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description  启动类
+ * @author gcc
+ * Date 2023/3/15 16:52
+ */
+@SpringBootApplication
+public class MaterialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialsApplication.class, args);
+    }
+
+}
